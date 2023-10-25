51893045dc63f7c66f02201516d29d3e335951ad
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-10-25 18:43:16 -0300

    Primer Commit

---
+++ my-project/src/Componentes/Cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import data from "../data.json";
+
+export default function Cards() {
+  return (
+    <div className="flex flex-col items-center w-full m-3">
+      <h1 className="text-3xl font-extrabold text-gray-900 sm:text-4xl m-2 mb-10 uppercase text-center">
+        Reserva tu cancha <br /> favorita por WhatsApp
+      </h1>
+
+      <div className="flex flex-col space-y-4">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="border bg-white shadow-md rounded-md w-96"
+          >
+            <a
+              href={item.whatsappLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block p-6 m-2 text-2xl font-medium text-gray-700 hover:bg-gray-50 text-center"
+            >
+              {item.name}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ my-project/src/Componentes/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-screen-xl px-4 pb-8 pt-16 sm:px-6 lg:px-8 lg:pt-24 mb-10">
+        <div className="text-center">
+          <h2 className="text-2xl font-extrabold text-gray-900 sm:text-5xl">
+            ¿QUERES AGREGAR TU CANCHA?
+          </h2>
+          <a
+            href="https://wa.me/5492235916616"
+            className="mt-8 inline-block rounded-full border px-12 py-3 text-base font-medium text-white bg-black  focus:outline-none focus:ring active:bg-white active:text-black"
+          >
+            ESCRIBINOS
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
